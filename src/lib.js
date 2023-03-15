2a0e8be418e5934511d8e1a3be7cc766882083df
intertext.no <orstavik77@gmail.com>
2023-03-15 12:37:26 +0100

    removing the old :on / ownerCallback reaction. This is now done via dotReactions

--- src/lib.js
+++ src/lib.js
@@ -51,22 +51,6 @@
     return strWithDash.replace(/-([a-z])/g, g => g[1].toUpperCase());
   }
 
-  function ownerCallback(e, _, ...methods) {
-    const element = this.ownerElement;
-    //1. get all the methods. If one method is missing, the reaction throws
-    if (!methods.length) methods = this.value.split(" ").map(toCamelCase);
-    methods = methods.map(method => {
-      if (!(method in element))
-        throw `'.${method}' is not a function on element <${element.tagName}>. Is it a typo?`;
-      return method;
-    });
-    //2. run each method one by one
-    for (let method of methods)
-      element[method](e);
-    //3. don't forget to return the e!
-    return e;
-  }
-
   function cssClass(e, css, onOff) {
     if (onOff === undefined || onOff === "on")
       this.ownerElement.classList.add(css);
@@ -90,7 +74,6 @@
     dispatchClone, //todo untested
     hasKey: (e, prefix) => (e[prefix + "Key"]) ? e : undefined,
     once,
-    ownerCallback,
     cssClass,
     toCamelCase,
     fetch: _fetch,  //todo untested
